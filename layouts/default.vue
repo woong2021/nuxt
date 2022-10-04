11e2431d13b6636b73db7bcc14c07e11980c5237
변세웅 <byeonse-ung@byeonse-ung-ui-MacBookPro.local>
2022-10-04 16:36:14 -0700

    초기샛팅

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header>
+      <h1>{{ $route.name }} 페이지</h1>
+      <NuxtLink to="/">홈 페이지</NuxtLink>
+      <NuxtLink to="/main">메인 페이지</NuxtLink>
+      <NuxtLink to="/product">상품 페이지</NuxtLink>  
+    </header> 
+    <Nuxt />
+  </div>
+</template>
+
---
+++ pages/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Logo />
+    <p>메인페이지 입니다.</p>
+  </div>
+</template>
+
+<script>
+import Logo from '../components/NuxtLogo.vue';
+export default {
+  components:{
+    Logo,
+  }
+}
+</script>
+
+<style>
+
+</style>
